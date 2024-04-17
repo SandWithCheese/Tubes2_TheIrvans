07b0e761586698142f47aad9315a5e50d61753f6
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2024-04-17 14:00:25 +0700

    update navbar

--- src/frontend/src/routes/__root.tsx
+++ src/frontend/src/routes/__root.tsx
@@ -4,26 +4,29 @@ const Navbar = () => {
   return (
     <div>
       <div
-        className="shadow-inner-2xl  font-Lobster flex flex-row justify-between bg-background p-2 text-foreground transition-opacity duration-500 ease-in-out"
+        className="shadow-inner-2xl  font-Lobster flex flex-row justify-between bg-primary  text-foreground transition-opacity duration-500 ease-in-out"
         data-replace='{ "opacity-0": "opacity-100" }'
       >
-        <Link to="/" className="hover:font-bold">
-          <div className="flex flex-row">
+        <Link to="/" className=" p-2">
+          <div className="flex flex-row transition hover:scale-90 active:scale-75">
             <img
-              className=" flex w-12 items-center"
+              className=" flex w-12 items-center "
               src="/logo.png"
               alt="logo"
             />
             <div>
-              <div className="ml-4">
+              <div className="ml-4 ">
                 <div>Tugas Besar 2 Stima</div>
-                <div className=" font-Akaya flex">By TheIrvans</div>
+                <div className=" font-Akaya flex  ">By TheIrvans</div>
               </div>
             </div>
           </div>
         </Link>
 
-        <Link to="/about" className="flex hover:font-bold ">
+        <Link
+          to="/about"
+          className="flex transition hover:scale-90 active:scale-75 "
+        >
           <div className="mr-4  flex items-center">
             <img src="/info.png" alt="info logo" className="shadow-2xl" />
             <div className=" ml-2 shadow-2xl ">About</div>
